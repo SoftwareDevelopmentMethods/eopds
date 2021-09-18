45a03bec0332735963469396656f83aa8c9eb3c3
Sergey Germanov <sergyleo@yahoo.com>
2021-09-18 20:13:40 +1200

    onboarder page 1st draft

---
+++ src/Profile.js
@@ -0,0 +1,12 @@
+function Profile(props) {
+    return (
+        <div class="card" style={{ width: "18rem" }}>
+            <img src='whiskey.jpg' class="card-img-top" alt="Whiskey Onboarder" />
+            <div class="card-body">
+                <p class="card-text">Best Onboarder Ever!</p>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
